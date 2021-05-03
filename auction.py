ccff836784d74a78140a5951f8ddb8ea71366776
Geekycoder12 <supreet.singh.cse19@itbhu.ac.in>
2021-05-03 15:15:59 +0530

    algo 1 completed

--- auction.py
+++ auction.py
@@ -10,25 +10,36 @@ C = []
 for i in range(m):
     temp = []
     for j in range(n):
-        temp.append(int(input("Enter bid of model owner {} for user {}:".format(i+1,+1))))
+        temp.append(int(input("Enter bid of model owner {} for user {}:".format(i+1,j+1))))
     C.append(temp)
 
 D =[]
 for i in range(n):
-    D.append(int(input("Enter Demand of User {}".format(i+1))))
+    D.append(int(input("Enter Demand of User {}:".format(i+1))))
 
 sorted(D)
  
 p = math.ceil((n+1)/2)
 x = 0
-V = []
+V = {}
 R = []
 for i in range(m):
+    V[i+1] = []
+    temp = 0
     for j in range(n):
-        if(C[i][j] > D[p] and D[j] < D[p]):
+        if(C[i][j] > D[p-1] and D[j] <= D[p-1]):
             #V.append(i+1,j+1)
-            V.append(j+1)
-            R.append(i+1)
+            V[i+1].append(j+1)
+            temp = 1
             x = min(x,C[i][j])
-
+        if(temp):
+            R.append(j+1)
+print(V)
+print(R)
+print(x)
+ #Algorithm 2 
+X = []
+Y = R
+P = []
+Q = []
  
